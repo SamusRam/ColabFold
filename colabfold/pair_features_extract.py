3c3ef692ad423e2eb5977cb778a22e1e4404cd25
Raman Samusevich <raman.samusevich@honeywell.com>
2021-11-20 18:55:11 +0100

    script for pair features extraction: making tf and jax not to block the whole gpu memory

--- colabfold/pair_features_extract.py
+++ colabfold/pair_features_extract.py
@@ -5,6 +5,12 @@ from colabfold.batch import get_queries, run
 import argparse
 import hashlib
 from pathlib import Path
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
 
 def add_hash(x,y):
