22aeba161a8650b0b17e42b24b7424d70c70cf37
Raman Samusevich <raman.samusevich@honeywell.com>
2022-01-10 14:07:08 +0100

    separate msa extraction polishing

--- colabfold/msa_precomputation_coordination.py
+++ colabfold/msa_precomputation_coordination.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import subprocess
 import argparse
-import numpy as np
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--start-perc', type=float, default=0)
 parser.add_argument('--end-perc', type=float, default=100)
+parser.add_argument('--data-root', type=str)
 args = parser.parse_args()
 
-tps_df = pd.read_excel('data/TPS-database_2021_11_04.xlsx', engine='openpyxl')
-rf_df = pd.read_excel('data/tps_detection_plants_new_proteins_df.csv')
+tps_df = pd.read_excel(os.path.join(args.data_root, 'TPS-database_2021_11_04.xlsx'), engine='openpyxl')
+rf_df = pd.read_excel(os.path.join(args.data_root, 'tps_detection_plants_new_proteins_df.csv'))
 df = pd.concat((tps_df[['Uniprot ID', 'Amino acid sequence']], rf_df[['Uniprot ID', 'Amino acid sequence']]))
 df = df.sort_values(by='Uniprot ID')
 start_i = int(len(df)*args.start_perc/100)
@@ -24,3 +25,4 @@ for _, row in df.iloc[start_i: end_i].iterrows():
 
 
 # pip install "colabfold[alphafold] @ git+https://github.com/SamusRam/ColabFold@high_quality_representations"
+# pip install --upgrade --force-reinstall "colabfold[alphafold] @ git+https://github.com/SamusRam/ColabFold@high_quality_representations"
